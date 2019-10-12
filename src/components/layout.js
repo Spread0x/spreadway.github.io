db67d28964164c4ded49dca121124a977cce6fa6
Stanislav Sobolev <iamjacke@gmail.com>
2019-10-12 03:48:34 +0300

    chore: refactoring

--- src/components/layout.js
+++ src/components/layout.js
@@ -53,34 +53,8 @@ const Layout = ({ children, headerClassName }) => (
       <link href="/stylesheets/styles.css" rel="stylesheet" media="screen" />
       <link rel="stylesheet" href="https://s.pageclip.co/v1/pageclip.css" media="screen" />
       <script src="https://s.pageclip.co/v1/pageclip.js" charset="utf-8" />
-      <script src="/javascripts/lib/jquery/jquery.js" type="text/javascript"></script>
-      <script src="/javascripts/js/site.js" type="text/javascript"></script>
       <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.9"></script>
       <script src="https://cdn.jsdelivr.net/npm/jquery.scrollto@2.1.2/jquery.scrollTo.min.js"></script>
-      <script>
-      {`
-        $( document ).ready(function() {
-                  var typed = new Typed("#indexheadertypings", {
-                  strings: ['job', 'task', 'startup', 'project'],
-                  typeSpeed: 100,
-                  backSpeed: 50,
-                  backDelay: 2000,
-                  startDelay: 2000,
-                  loop: true,
-                  onComplete: function(self) { console.log('onCmplete ' + self) },
-                  preStringTyped: function(pos, self) { console.log('preStringTyped ' + pos + ' ' + self); },
-                  onStringTyped: function(pos, self) { console.log('onStringTyped ' + pos + ' ' + self) },
-                  onLastStringBackspaced: function(self) { console.log('onLastStringBackspaced ' + self) },
-                  onTypingPaused: function(pos, self) { console.log('onTypingPaused ' + pos + ' ' + self) },
-                  onTypingResumed: function(pos, self) { console.log('onTypingResumed ' + pos + ' ' + self) },
-                  onReset: function(self) { console.log('onReset ' + self) },
-                  onStop: function(pos, self) { console.log('onStop ' + pos + ' ' + self) },
-                  onStart: function(pos, self) { console.log('onStart ' + pos + ' ' + self) },
-                  onDestroy: function(self) { console.log('onDestroy ' + self) }
-                });
-        });
-      `}    
-      </script>  
       </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} headerClassName={headerClassName} />
         <div className="page-wrapper">
